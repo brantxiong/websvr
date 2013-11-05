337bbdebf5315388128a2aaa9450f6738c2ce0ee
Alexbob <hustxiongbo@gmail.com>
2013-11-05 16:00:02 +0800

    add svr.c

---
+++ src/head/svr.h
@@ -0,0 +1,8 @@
+#ifndef _SVR_H_
+#define _SVR_H_
+
+void MainServer(char *dir, int port);
+void startServer(int port);
+void respond(int n, char *dir);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,19 +1,15 @@
 #include "head/unp.h"
 #include "head/init.h"
 #include "head/conf.h"
-#include "head/log.h"
+#include "head/svr.h"
 #define MAX_DIR_LENGTH 50
 
 int main(int argc, char const *argv[])
 {
     char *dir = (char *) malloc(sizeof(char) * (MAX_DIR_LENGTH));
-    int port;
     ReadServerDir(dir);
-    port = ReadServerPort();
-    printf("dir: %s\n", dir);
-    printf("port: %d\n", port);
-    AcessLog("127.0.0.1", "test");
-    ErrorLog("WARNING", "fail to bind()");
-    test();
+
+    MainServer(dir, ReadServerPort());
+
     return 0;
 }
